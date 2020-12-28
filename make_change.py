a5c3313b7ce8fb5fc1de19144458d8f7a0b6c8e8
Melina Lopez <38011974+Melinal23@users.noreply.github.com>
2020-12-28 15:05:50 -0800

    Add files via upload

---
+++ make_change.py
@@ -0,0 +1,19 @@
+def make_change(coins, amount):
+
+  matrix = [[0 for _ in range(amount+1)] for _ in range(len(coins))]
+
+  for i in range(len(coins)): # base case, amount = 0
+    matrix[i][0] = 1
+
+  for row in range(len(coins)):
+    for col in range(1, amount+1):
+      if(coins[row] <= col):
+        matrix[row][col] = (matrix[row - 1][col] + matrix[row][col - coins[row]])
+      else:
+        matrix[row][col] = matrix[row - 1][col]
+
+  return matrix[-1][-1] # the last element in the matrix
+
+print(make_change([2,3,5,10], 15))
+print(make_change([1,2,5], 5))
+print(make_change([1,5], 6))
